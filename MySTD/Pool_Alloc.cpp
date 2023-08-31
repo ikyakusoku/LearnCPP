fad8b0103e32e4750e59ea1e328d8f471ba63c1a
ikyakusoku <88141286+ikyakusoku@users.noreply.github.com>
2023-09-01 00:30:02 +0800

    freelist方法的内存池
    写的并不愉快

---
+++ MySTD/Pool_Alloc.cpp
@@ -0,0 +1,108 @@
+#include "Pool_Alloc.h"
+
+void* PoolAlloc::refill(size_t bytes)
+{
+	size_t nobjs = NOBJS;//表示实际申请了多少块
+	//首先从池中去一块内存
+	char* chunk = static_cast<char*>(chunk_alloc(bytes,nobjs));
+	if (nobjs == 1)
+	{
+		return chunk;
+	}
+	//取出一块内存
+	size_t index = FreeList_Index(bytes);
+	obj* result = (obj*)chunk;
+	free_list[index] = (obj*)(chunk+bytes);
+	//剩下的内存切块
+	obj* current_obj = nullptr;//当前块
+	obj* next_obj = free_list[index];//下一块
+	for (int i = nobjs - 1;; i--) {
+		current_obj = next_obj;
+		next_obj = (obj*)((char*)next_obj + bytes);
+		if (i == 1) {
+			current_obj->next = nullptr;
+			break;
+		}
+		else {
+			current_obj->next = next_obj;
+		}
+	}
+	return result;
+}
+
+void* PoolAlloc::chunk_alloc(size_t bytes, size_t& nobjs)
+{
+	//根据需要申请的内存与战备池中的内存作比较，判断是否重新申请
+	size_t totalSize = bytes * NOBJS;
+	size_t leftSize = end_free - start_free;
+
+	if (totalSize <= leftSize)
+	{
+		//剩余战备池内存足够，就分配出去totalSize
+		auto result=start_free;
+		start_free += totalSize;
+		return result;
+	}
+	else if (bytes <= leftSize) {
+		//剩余战备池内存只足大于1不到NOBJS个，那么取出一块，剩下的不管。
+		nobjs = leftSize / bytes;
+		totalSize = nobjs * bytes;
+		auto result = start_free;
+		start_free = start_free + totalSize;
+		return result;
+	}
+	else {
+		//剩余战备池内存不够一个了
+		
+		//如果有剩余的，先把剩下的内存挂到对应的freelist下面
+		if (leftSize > 0) {
+			auto index=FreeList_Index(leftSize);
+			((obj*)start_free)->next = free_list[index];
+			free_list[index] = (obj*)start_free;
+		}
+		//然后申请内存大小为2倍
+		size_t bytes_to_get = 2 * totalSize + RoundUp(heap_size >> 4);//roundup()这里加的到底是什么阿
+		start_free = (char*)malloc(bytes_to_get);
+		//参考文章和代码中，判断start_free不先置为空，那如果我战备池有残余，但内存申请失败了呢？可见malloc估计是分配失败会返回空指针。
+		if (!start_free) {
+			//如果申请失败，没内存了，只好在之前已经分配的内存中拿一块出来，作为战备池
+			//这里参考的文章以及TinySTL的写法有点问题，应该向后面那些挂着更大内存块的freelist中找内存，而不是整个遍历，
+			//如果我找到的是比bytes小的，再次chunk_alloc()，还是会回到这里的窘境。等于白做了
+			for (size_t i = bytes; i <= MAXBYTES; i += ALIGN) {
+				size_t index = FreeList_Index(i);
+				obj* curList = free_list[index];
+				if (curList != nullptr) {
+					free_list[index]=curList->next;
+					start_free = (char*)curList;
+					end_free = start_free + i;
+					return chunk_alloc(bytes, nobjs);
+				}
+			}
+			//这里参考文章写的也有问题，没有抛出异常也没有对应的返回，又是死循环，不断递归.我这里简单处理成立返回空指针，至少不会不停递归。
+			end_free = 0;
+			return nullptr;
+		}
+		//如果申请成功就直接返回，让refill切割
+		heap_size += bytes_to_get;
+		end_free = start_free + bytes_to_get;
+		return chunk_alloc(bytes,nobjs);//这递归调用一下处理一个start_free
+	}
+}
+
+void* PoolAlloc::allocate(size_t bytes)
+{
+	//首先判断申请的大小，超出范围就不负责了(交给malloc)
+	if (bytes > 128)
+	{
+		return malloc(bytes);
+	}
+	//先看一下对齐之后，分配多大内存，并挂在哪个freelist上
+	size_t index = FreeList_Index(bytes);
+	obj* listHead = free_list[index];
+	if (listHead != nullptr)
+	{
+		free_list[index] = listHead->next;
+		return listHead;
+	}
+	return refill(RoundUp(bytes));
+}
---
+++ MySTD/Pool_Alloc.h
@@ -0,0 +1,58 @@
+#pragma once
+#include<stdlib.h>
+
+//小结：内存池参考了一些文章、TinySTL以及侯捷的c++内存管理中对SGI中内存池实现方案的讲解。
+//内存池理论本身还是很好理解的，和线程池，对象池一样，一来节省反复申请分配内存的开销；二来提高了空间利用率，
+//因为malloc本身申请的内存带有cookie和debug用的信息。不管怎么说方法就是提前申请一堆，之后直接取着用，用完了再
+//返回到池子中。当然在具体的设计中，使用的是数组存放的单列表，所谓freelist们。为了统一管理所有的内存分配，所以
+//有了对应多种内存大小的freelist可选。现在大家都用统一的分配器分配了，而不是像allocator一样每个不同的类会有不
+//同的分配器实现。还有就是内存扩散(memory diffusion),这里freelist的方法并没有解决，以及内存碎片，说到底对齐产
+//生的内部碎片，这个方法没办法避免。
+//当然最后还是想吐槽一下，自己参考的东西稍微有点老了，接口设计给我的感觉总是怪怪的，比如refill和chunk_alloc()，
+//说实话，这个接口参数让我挺不舒服的，一会有要调用者考虑传入的是否是一个对齐过的内存一会又不用，以及把块的多少
+//作为引用返回过来，这个接口从原型上理解起来多少让我觉得不舒服，不过考虑到它是private，也不需要用户知道什么，内
+//部的函数划分怎么舒服怎么来了。
+//同时参考TinySTL过程中看到一些错误，并且感觉也是参考SGI写的。
+//加上事先看了关于实现高并发内存池的文章，觉得这东西太过鸡肋了点，做不了高性能的并发（如果只是对接口都上锁，那
+//开销也太大了），再加上STL中allocator的设计本身的一些问题，这种种让我整个写代码的过程一点也不畅快，还剩下一点，
+//虽然想善始善终，但之后估计又不会对剩下reallocate和deallocate的部分做实现了，本身很简单没什么障碍，当然至少今
+//天是不想碰了。
+
+class PoolAlloc {
+private:
+	//3个成员变量abc,a要用bc初始化，bc值初始化，他们最后会被初始化为什么
+	static constexpr size_t ALIGN = 8;
+	static constexpr size_t MAXBYTES = 128;
+	static constexpr size_t NFREELISTS = MAXBYTES / ALIGN;
+	static constexpr size_t NOBJS = 20;
+private:
+	//内嵌指针
+	struct obj{
+		obj* next;
+	};
+	//freelist指针数组，每个元素为指向指定对齐大小内存的freelist
+	static obj* free_list[NFREELISTS];
+private:
+	static char* start_free;
+	static char* end_free;
+	static size_t heap_size;//这是？？
+private:
+	static size_t RoundUp(size_t bytes) {
+		return (bytes + ALIGN - 1) & ~(ALIGN - 1);
+	}
+	//通过上调后的内存确定申请的内存挂到那个freelist下面
+	static size_t FreeList_Index(size_t bytes) {
+		return (bytes - 1) / ALIGN;
+	}
+	//负责将将空间切块,参数为对齐后的内存大小
+	static void* refill(size_t bytes);
+
+	//负责申请一大块可以被指定bytes大小的对象利用空间,第二个参数用于返回申请空间的块数，
+	//也就是说这个接口设计完全不考虑切块，只是想办法返回一大块可以容纳bytes大小的内存
+	static void* chunk_alloc(size_t bytes, size_t& nobjs);
+public:
+	static void* allocate(size_t bytes);
+	static void  deallocate(void* ptr, size_t bytes);
+	static void* reallocate(void* ptr, size_t old_sz, size_t new_sz);
+
+};
